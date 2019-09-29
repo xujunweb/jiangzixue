02f5361b141edd390a8f1fe9b9da15fc772a94c4
xujun <xujun@youanmi.com>
2019-09-29 20:53:37 +0800

    迭代

--- src/config/index.js
+++ src/config/index.js
@@ -16,6 +16,7 @@ export default {
     // dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
     // dev: 'http://120.79.240.245:8086/',
     dev: '',
-    pro: 'http://120.79.240.245:8086/website/'
+    // pro: 'http://120.79.240.245:8086/website/'
+    pro: 'http://www.jiangzixue666.com/website/'
   }
 }
